0a95844d2837e83dc814bf353f44e736d348ba9a
Bogumil Bierc <bogumil.bierc@gmail.com>
2025-07-26 19:53:05 +0200

    Add reading and displaying logic

--- egt_display/egt_display.ino
+++ egt_display/egt_display.ino
@@ -1,42 +1,52 @@
 /**
 * Arduino EGT Display
 */
+#define DEBUG_ENABLED false
 
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
 #include <max6675.h>
 
+const int THERMO_SLAVE_OUT = 12;
+const int THERMO_CLOCK = 13;
+
+const int THERMO_CS_BANK_1 = 7;
+const int THERMO_CS_BANK_2 = 8;
+const int THERMO_CS_BANK_3 = 9;
+const int THERMO_CS_BANK_4 = 10;
+
+char intStringBuff[4];
+
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
-int thermoSO = 12;
-int thermoCS = 10;
-int thermoSCK = 13;
-
-MAX6675 thermocouple(thermoSCK, thermoCS, thermoSO);
+MAX6675 thermocoupleBank1(THERMO_CLOCK, THERMO_CS_BANK_1, THERMO_SLAVE_OUT);
+MAX6675 thermocoupleBank2(THERMO_CLOCK, THERMO_CS_BANK_2, THERMO_SLAVE_OUT);
+MAX6675 thermocoupleBank3(THERMO_CLOCK, THERMO_CS_BANK_3, THERMO_SLAVE_OUT);
+MAX6675 thermocoupleBank4(THERMO_CLOCK, THERMO_CS_BANK_4, THERMO_SLAVE_OUT);
 
 void setup() {
   // put your setup code here, to run once:
   setupScreen();
 
+#if DEBUG_ENABLED == true
   Serial.begin(9600);
-
   Serial.println("MAX6675 test");
+#endif
+
   // wait for MAX chip to stabilize
   delay(500);
+
+  // set all chips high
+  setAllPinsToHigh();
 }
 
 void loop() {
-  // basic readout test, just print the current temp
+  printFirstLine();
+  printSecondLine();
 
-   Serial.print("C = ");
-   Serial.println(thermocouple.readCelsius());
-   Serial.print("F = ");
-   Serial.println(thermocouple.readFahrenheit());
-
-   // For the MAX6675 to update, you must delay AT LEAST 250ms between reads!
-   delay(1000);
-  // put your main code here, to run repeatedly:
+  setAllPinsToHigh();
+  delay(1000);
 }
 
 void setupScreen() {
@@ -49,8 +59,51 @@ void setupScreen() {
 void printSplashScreen() {
   lcd.home();
   lcd.print("EGT...");
-  delay(200);
+  delay(100);
   lcd.setCursor(0, 1);  //jump to new line
   lcd.print("        ...QUACK");
-  delay(200);
+  delay(100);
+  lcd.clear();
+}
+
+void printFirstLine() {
+  lcd.home();
+  lcd.print("T1:");
+  printFourDigitInt(getEgt(thermocoupleBank1, 1));
+
+  lcd.print(" T2:");
+  printFourDigitInt(getEgt(thermocoupleBank2, 2));
+}
+
+void printSecondLine() {
+  lcd.setCursor(0, 1);  //jump to new line
+  lcd.print("T3:");
+  printFourDigitInt(getEgt(thermocoupleBank3, 3));
+
+  lcd.print(" T4:");
+  printFourDigitInt(getEgt(thermocoupleBank4, 4));
+}
+
+void printFourDigitInt(int value) {
+  sprintf(intStringBuff, "%4d", value);
+  lcd.print(intStringBuff);
+}
+
+int getEgt(MAX6675 thermocouple, int bankIndex) {
+  float tempCelsius = thermocouple.readCelsius();
+
+#if DEBUG_ENABLED == true
+  Serial.print("EGT");
+  Serial.println(bankIndex);
+  Serial.println(tempCelsius);
+#endif
+
+  return int(trunc(tempCelsius));
+}
+
+void setAllPinsToHigh() {
+  digitalWrite(THERMO_CS_BANK_1, HIGH);
+  digitalWrite(THERMO_CS_BANK_2, HIGH);
+  digitalWrite(THERMO_CS_BANK_3, HIGH);
+  digitalWrite(THERMO_CS_BANK_4, HIGH);
 }
